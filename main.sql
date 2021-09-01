737238fbf7fc8a6b4ad693b8deb5ef80a88e392a
NAOHIROITO2178 <87390471+NAOHIROITO2178@users.noreply.github.com>
2021-09-01 23:27:53 +0900

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXIST TECH-COMMUNITY;
 CREATE TABLE TECH-COMMUNITY(
    username VARCHAR(12),
-   e-mail VARCHAR(),
-   password VARCHAR(); 
+   e-mail VARCHAR(30),
+   password VARCHAR()
 );
 
 INSERT INTO TECH-COMMUNITY(username,e-mail,password) VALUES;
-(NAOHIRO,e-mail1,1234);
-(NAO,e-mail2,3456);
+(NAOHIRO,e-mail1,1234),
+(NAO,e-mail2,3456),
 (NARO,e-mail3,5678);
 
 SELECT * FROM TECH-COMMUNITY WHERE username;
